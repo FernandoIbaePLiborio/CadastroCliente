e1aa2d18665ba16f6847d5314ed8f7b67f2188e5
FernandoIbaePLiborio <fernando.liborio@opet.edu.br>
2021-08-10 15:46:57 -0300

    Delete Cliente directory

--- Cliente/src/main/java/io/github/profile/fatory/ServiceFactory.java
+++
@@ -1,9 +0,0 @@
-package io.github.profile.fatory;
-
-import javax.naming.NamingException;
-
-public interface ServiceFactory {
-
-	public <T> T getService(final String jndiName) throws IllegalArgumentException, NamingException;
-
-}
--- Cliente/src/main/java/io/github/profile/ws/Aplicacao.java
+++
@@ -1,6 +0,0 @@
-package io.github.profile.ws;
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/WS")
-public class Aplicacao extends Application { }
