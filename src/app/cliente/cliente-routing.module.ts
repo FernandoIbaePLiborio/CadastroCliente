177ab396e88c4fbb905c9ce80ccfd6b390b9eb05
FernandoIbaePLiborio <fernando.liborio@opet.edu.br>
2021-08-10 19:39:01 -0300

    organização

--- src/app/cliente/cliente-routing.module.ts
+++ src/app/cliente/cliente-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from '../home/home.component';
 import { ClienteFormComponent } from './cliente-form/cliente-form.component';
 import { ClientesComponent } from './clientes/clientes.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'cliente-form', component: ClienteFormComponent },
   { path: 'clientes', component: ClientesComponent }
 ];
